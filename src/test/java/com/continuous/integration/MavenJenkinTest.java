0c1d5cf7dc6058ca5cb38c7910cc0e03c6cd825d
vinodtestengineer1480 <vinodtestengineer1480@gmail.com>
2021-02-24 18:50:57 +0530

    first commit

---
+++ src/test/java/com/continuous/integration/MavenJenkinTest.java
@@ -0,0 +1,11 @@
+package com.continuous.integration;
+
+import org.junit.Test;
+
+public class MavenJenkinTest {
+	@Test
+	public void mavenJenkinsIntegrationTest() {
+		System.out.println("This test is verify the maven and jenkins configuration setup");
+	}
+
+}
