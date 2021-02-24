741e95352a9fa87dc312b613ffd55db704179478
vinodtestengineer1480 <vinodtestengineer1480@gmail.com>
2021-02-24 19:23:05 +0530

    added 3 more tests

--- src/test/java/com/continuous/integration/MavenJenkinTest.java
+++ src/test/java/com/continuous/integration/MavenJenkinTest.java
@@ -4,8 +4,24 @@ import org.junit.Test;
 
 public class MavenJenkinTest {
 	@Test
-	public void mavenJenkinsIntegrationTest() {
+	public void mavenJenkinsIntegrationTest1() {
 		System.out.println("This test is verify the maven and jenkins configuration setup");
 	}
+	
+	@Test
+	public void mavenJenkinsIntegrationTest2() {
+		System.out.println("This test is verify the maven and jenkins configuration setup");
+	}
+
+	@Test
+	public void mavenJenkinsIntegrationTest3() {
+		System.out.println("This test is verify the maven and jenkins configuration setup");
+	}
+
+	@Test
+	public void mavenJenkinsIntegrationTest4() {
+		System.out.println("This test is verify the maven and jenkins configuration setup");
+	}
+
 
 }
